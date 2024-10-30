a2bf5eacd43d15fd1e2ec8e1d67074a95ebbb6cf
Shrey <chowdhuryshreyansh@yahoo.com>
2024-10-30 16:49:13 +0800

    Ashdfahuhasudlahsdsadaedasd
    Co-authored-by: Computhink-Alfred <Computhink-Alfred@users.noreply.github.com>

--- Shreyansh-WebThinker1-Students-Copy/script/contacts.js
+++ Shreyansh-WebThinker1-Students-Copy/script/contacts.js
@@ -1,3 +1,48 @@
 let validate = () => {
-	// Fill in the necessary validation here
+	let name = document.getElementById('contactName');
+	if (name.value.length != 0) {
+		name.classList = "form-control";
+		document.getElementById('contactNameFeedback').classList = "d-none text-danger";
+	} else {
+		name.classList = "form-control is-invalid";
+		document.getElementById('contactNameFeedback').classList = "text-danger";
+	}
+	
+	let email = document.getElementById('contactEmail');
+	// console.log (email.value);
+	if (email.value.length != 0) {
+		email.classList = "form-control";
+		document.getElementById('contactEmailFeedback').classList = "d-none text-danger";
+	} else {
+		email.classList = "form-control is-invalid";
+		document.getElementById('contactEmailFeedback').classList = "text-danger";
+	}
+
+	if (!email.value.includes('@')) {
+		email.classList = "form-control is-invalid";
+		document.getElementById('contactEmailFeedback').classList = "text-danger";
+	} else {
+		email.classList = "form-control";
+		document.getElementById('contactEmailFeedback').classList = "d-none text-danger";
+	}
+	
+	let subject = document.getElementById('contactSubject');
+	if (subject.value.length > 5) {
+		subject.classList = "form-control";
+		document.getElementById('contactSubjectFeedback').classList = "d-none text-danger";
+	} else {
+		subject.classList = "form-control is-invalid";
+		document.getElementById('contactSubjectFeedback').classList = "text-danger";
+	}
+	
+	let message = document.getElementById('contactMessage');
+	if (message.value.length > 20) {
+		message.classList = "form-control";
+		document.getElementById('contactMessageFeedback').classList = "d-none text-danger";
+	} else {
+		message.classList = "form-control is-invalid";
+		document.getElementById('contactMessageFeedback').classList = "text-danger";
+	}
+
+	if (document.querySelectorAll('.is-invalid').length === 0) window.location.reload();
 }
--- Shreyansh-WebThinker1-Students-Copy/script/projects.js
+++ Shreyansh-WebThinker1-Students-Copy/script/projects.js
@@ -1,1 +1,62 @@
-// 
+// ctx.beginPath ();
+    // ctx.moveTo (0,0);
+    // ctx.lineTo (100, 0);
+    // ctx.stroke ();
+
+    // ctx.fillStyle = "rgba(255, 0, 0, 0.314159265358559)";
+    // ctx.fillRect (10, 10, 100, 100);
+
+    // ctx.fillStyle = "rgb(0, 255, 0)";
+    // ctx.fillRect (350, 250, 100, 100);
+
+    // ctx.fillStyle = "rgb(0, 0, 255)";
+    // ctx.fillRect (690, 480, 100, 100);
+
+    // ctx.beginPath ();
+    // ctx.arc (120, 120, 100, 0, 2 * Math.PI);
+    // ctx.fillStyle = "rgb (0, 0, 0)";
+    // ctx.stroke ();
+    // ctx.fill ();
+
+let x, y, dx, dy, canvas, ctx, ballRadius;
+
+ // erase previous frame an draw new frame
+ const updateCanvas = () => {
+    // erase canvas
+    ctx.clearRect (0, 0, canvas.width, canvas.height)
+
+    ctx.beginPath();
+    ctx.fillStyle = "rgb(0, 0, 255)";
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fill ();
+    ctx.closePath ();
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+    x = x + dx;
+    y = y + dy;
+
+    requestAnimationFrame(updateCanvas);
+}
+
+window.onload = () => {
+    canvas = document.getElementById("canvasBoard");
+    ctx = canvas.getContext('2d');
+
+     ballRadius = 10;
+
+    x = canvas.width/2 - ballRadius / 2;
+    y = canvas.height/2 - ballRadius/2;
+    dx = 2;
+    dy = 2;
+
+    updateCanvas();
+
+
+}
